ebb8dfcf0e9b0ee5916ed8976d3f78f18a9158d8
tiosaad <74604457+tiosaad@users.noreply.github.com>
2021-02-08 15:09:33 -0300

    Add files via upload

---
+++ src/1/help.js
@@ -0,0 +1,74 @@
+const help = (prefix) => {
+ return `
+  ╔════════════════════
+  ║       ♐SAAD BOT♐
+  ╠════════════════════
+
+
+➸ Prefix:  *「${prefix} 」*
+➸ Status: *「 Online 」*
+
+ *「Comandos de Figurinhas」*
+ 
+👉 *${prefix}.sticker* *${prefix}.stiker*
+
+👉 *${prefix}.stickernobg* *${prefix}.stikernobg*
+
+👉 *${prefix}.toimg*
+
+👉 *${prefix}.tsticker* *${prefix}.tstiker*
+
+ *「Comandos dos Memes」*
+
+👉 *${prefix}.meme*
+
+👉 *${prefix}.memeindo*
+
+*「Outros Comandos」*
+
+👉 *${prefix}.gtts*
+
+👉 *${prefix}.loli*
+
+👉 *${prefix}.nsfwloli*
+
+👉 *${prefix}.url2img*
+
+👉 *${prefix}.simi*
+
+👉 *${prefix}.ocr*
+
+👉 *${prefix}.wait*
+
+👉 *${prefix}.setprefix*
+
+👉 *${prefix}.belle2*
+
+👉 *${prefix}.canal*
+
+👉 *${prefix}.loli1
+
+👉 *${prefix}.hentai
+
+👉 *${prefix}.porno
+
+*「Comandos de Grupos」*
+
+👉 *${prefix}.add*
+
+👉 *${prefix}.kick*
+
+👉 *${prefix}.promote*
+
+👉 *${prefix}.demote*
+
+👉 *${prefix}.linkgroup*
+
+👉 *${prefix}.leave*
+
+👉 *${prefix}.tagall*
+
+👉 *${prefix}.simih*
+}
+
+exports.help = help
